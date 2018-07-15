8bb3cb40d0e6b1a2f2d12ff8b91f42d902b7a551
Emanuel Faisca <emanuelfaisca@gmail.com>
2018-07-15 15:56:20 +0100

    Update

---
+++ CommonValidations/CommonUK.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Common Input Validations for English(UK) Data.
+/// (eg. Phone number, Postal code, ssn etc...)
+/// 
+/// Please follow single responsability pattern and naming convention.
+/// Please use, share, correct and update ;)
+/// </summary>
+/// 
+
+namespace CommonValidations
+{
+    class CommonUK
+    {
+
+    }
+}
